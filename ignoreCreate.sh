5ebc8112f1aae47d1810ce132c080f28775d7192
erguzel <erguzel@itu.edu.tr>
2018-11-01 23:27:14 +0100

    add ignore creator

---
+++ ignoreCreate.sh
@@ -0,0 +1,11 @@
+echo "*.classpath" > .gitignore
+echo "*.project" >> .gitignore
+echo ".settings/" >> .gitignore
+echo "*.springBeans" >> .gitignore
+echo "target/" >> .gitignore
+echo "gradle/" >> .gitignore
+echo "build/" >> .gitignore
+echo "out/" >> .gitignore
+echo ".gradle/" >> .gitignore
+echo ".idea/" >> .gitignore
+
