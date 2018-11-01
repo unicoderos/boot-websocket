5995eee61ef55ba050b3abfffb5af66ef154a44f
erguzel <erguzel@itu.edu.tr>
2018-11-01 20:49:08 +0100

    bulk initial commit

---
+++ src/main/java/com/javainuse/websocket/config/SocketTextHandler.java
@@ -0,0 +1,33 @@
+package com.javainuse.websocket.config;
+
+import java.io.IOException;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import java.util.Random;
+import java.lang.Math;
+@Controller
+@Component
+public class SocketTextHandler extends TextWebSocketHandler {
+int i=0;
+float max = 600;
+float min =100;
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message)
+			throws InterruptedException, IOException {
+
+		String payload = message.getPayload();/*retireve from object data*/
+		JSONObject jsonObject = new JSONObject(payload);
+		i=i+1;/*Counter for generating random number*/
+		Random rand = new Random();
+		double r =Math.round(((Math.random() * ((max - min) + 1)) + min)*100)/100;/* create Random Number */
+		session.sendMessage(new TextMessage(" " +r + "" + " "));
+	}
+	
+
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,69 @@
+var ws;
+function setConnected(connected) {
+	$("#connect").prop("disabled", connected);
+	$("#disconnect").prop("disabled", !connected);
+}
+
+function connect() {
+	ws = new WebSocket('ws://localhost:8080/user');
+	ws.onmessage = function(data) {
+		helloWorld(data.data);
+	}
+	setConnected(true);
+	console.log("connected");
+}
+
+function disconnect() {
+	if (ws != null) {
+		ws.close();
+	}
+	setConnected(false);
+	console.log("Websocket is in disconnected state");
+}
+
+function sendData(name) {
+	var data = JSON.stringify({
+		'user' :name
+	})
+	ws.send(data);
+}
+
+function helloWorld(message) {
+	i =0;
+	value = new Array(100);
+	tmp = $("#price").val();
+	console.log(tmp);
+	value[i]=message;
+	$("#price").val(message);
+	
+	if(tmp>value[i]){
+		
+		$("#price").css('color','red');
+		}
+	else{
+		console.log('yes');
+		$("#price").css('color','green');
+		
+	}
+	i++;
+	
+}
+
+$(function() {
+	$("form").on('submit', function(e) {
+		e.preventDefault();
+	});
+	$("#connect").click(function() {
+		connect();
+	});
+	$("#disconnect").click(function() {
+		disconnect();
+	});
+	$("#send").click(function() {
+		sendData();
+	});
+});
+$( document ).ready(function() {
+	  // Handler for .ready() called.
+	console.log("hello");
+	});
