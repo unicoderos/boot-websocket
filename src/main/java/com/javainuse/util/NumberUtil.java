15b139d46129be5f80212922c67c67326902221f
erguzel <erguzel@itu.edu.tr>
2018-11-04 03:55:26 +0100

    add number util

--- src/main/java/com/javainuse/util/NumberUtil.java
+++ src/main/java/com/javainuse/util/NumberUtil.java
@@ -1,4 +1,23 @@
 package com.javainuse.util;
 
+import java.util.Random;
+
+/**
+ * Contains number handling static methods
+ */
 public class NumberUtil {
+
+    /**
+     * Generates random number for given maximum and minimum interval
+     * @param max of the interval
+     * @param min of the interval
+     * @return random object
+     */
+    public static Object generateRandomNumber(int max, int min){
+
+        Random rand = new Random();
+        double randomN =Math.round(((Math.random() * ((max - min) + 1)) + min)*100)/100;/* create Random Number */
+        return randomN;
+    }
+
 }
