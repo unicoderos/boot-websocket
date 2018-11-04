1b25aee59bb49807572ac02b226a828ee4aa6c9a
erguzel <erguzel@itu.edu.tr>
2018-11-04 11:21:12 +0100

    add security config

--- src/main/java/com/javainuse/websocket/config/AppSecure.java
+++ src/main/java/com/javainuse/websocket/config/AppSecure.java
@@ -1,4 +1,40 @@
 package com.javainuse.websocket.config;
 
-public class AppSecure {
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@EnableAutoConfiguration
+@ComponentScan
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class AppSecure implements CommandLineRunner {
+
+    @Autowired
+    private AppService service;
+
+    @Override
+    public void run(String... args) throws Exception {
+        SecurityContextHolder.getContext()
+                .setAuthentication(new UsernamePasswordAuthenticationToken("user", "N/A",
+                        AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER")));
+        try {
+            System.out.println(this.service.secure());
+        }
+        finally {
+            SecurityContextHolder.clearContext();
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSecure.class, "--debug");
+    }
+
 }
--- src/main/java/com/javainuse/websocket/config/AppService.java
+++ src/main/java/com/javainuse/websocket/config/AppService.java
@@ -1,4 +1,25 @@
 package com.javainuse.websocket.config;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AppService {
+
+    @Secured("ROLE_USER")
+    public String secure() {
+        return "Connection Secured";
+    }
+
+    @PreAuthorize("true")
+    public String authorized() {
+        return "Connection Authorized";
+    }
+
+    @PreAuthorize("false")
+    public String denied() {
+        return "Connection Denied";
+    }
+
 }
