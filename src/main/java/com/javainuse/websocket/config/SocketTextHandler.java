32cb694cf516817dcb414d1765185506a2722edc
erguzel <erguzel@itu.edu.tr>
2018-11-04 11:22:27 +0100

    remove unused references

--- src/main/java/com/javainuse/websocket/config/SocketTextHandler.java
+++ src/main/java/com/javainuse/websocket/config/SocketTextHandler.java
@@ -3,14 +3,13 @@ package com.javainuse.websocket.config;
 import java.io.IOException;
 
 import com.javainuse.util.NumberUtil;
-import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import java.util.Random;
-import java.lang.Math;
+
+
 @Controller
 @Component
 public class SocketTextHandler extends TextWebSocketHandler {
@@ -26,7 +25,7 @@ public class SocketTextHandler extends TextWebSocketHandler {
 			throws InterruptedException, IOException {
 
 		String payload = message.getPayload();/*retireve from object data*/
-		JSONObject jsonObject = new JSONObject(payload);
+		//JSONObject jsonObject = new JSONObject(payload);
         Object randomN = NumberUtil.generateRandomNumber(600,100);
 		session.sendMessage(new TextMessage(" " +randomN.toString() + "" + " "));
 	}
