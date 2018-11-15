b31fcc7ad59c5dcbb83696b01a41bc8b28c762ad
erguzel <erguzel@itu.edu.tr>
2018-11-15 03:02:04 +0100

    add auto deploy to aws_01

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+copyToServer build/libs/boot-websocket-0.0.1-SNAPSHOT.war
+ssh -i ~/.ssh/olg-aws.pem ubuntu@18.223.10.187 <<EOF
+
+RUNPID=$(ps ax -l | grep "boot-websocket-0.0.1-SNAPSHOT.war" | awk '{print $4}' | head -n 1)
+kill -9 $RUNPID
+
+sudo nohup java -jar boot-websocket-0.0.1-SNAPSHOT.war &
+mv boot-websocket-0.0.1-SNAPSHOT.war boot-websocket-0.0.1-SNAPSHOT_$(date '+%d/%m/%Y_%H:%M:%S').war
+mv boot-websocket-0.0.1-SNAPSHOT_$(date '+%d/%m/%Y_%H:%M:%S').war back-up
+exit
+exit
+exit
+EOF
+exit
+
